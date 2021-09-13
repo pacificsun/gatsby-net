7d7e1e5d97b524e053e21a0e365f5f5551b96801
pacificsuraj <stsurajst@gmail.com>
2021-09-13 17:29:48 +0545

    pages, routes and 404 added

---
+++ src/pages/404.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+function NotFound() {
+  return (
+    <div>
+      <h2></h2>
+      <p>Sorry, That page does not exist !</p>
+    </div>
+  )
+}
+
+export default NotFound
---
+++ src/pages/projects/index.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+function projects() {
+  return (
+    <div>
+      <h2>Portfolio</h2>
+      <h3>Projects & Websites i've Created</h3>
+    </div>
+  )
+}
+
+export default projects
