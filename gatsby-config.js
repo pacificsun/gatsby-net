f107fb77b9d32c5ec1838508ad8c1a2e73830423
pacificsuraj <stsurajst@gmail.com>
2021-09-14 07:16:33 +0545

    shopify api integrated

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,8 +3,17 @@
  *
  * See: https://www.gatsbyjs.com/docs/gatsby-config/
  */
-
+require("dotenv").config()
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  plugins: [
+    {
+      resolve: "gatsby-source-shopify",
+      options: {
+        password: process.env.SHOPIFY_ADMIN_PASSWORD,
+        storeUrl: process.env.SHOPIFY_STORE_URL,
+      },
+    },
+    "gatsby-plugin-image",
+  ],
 }
